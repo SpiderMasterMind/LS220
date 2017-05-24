b0592ae5530cc80e330fa0c72c6b3961964c5640
Ben Stacey <benstacey@MacBook-Pro.local>
2017-05-24 18:05:44 +0100

    finish of small problems and 210 exercises

--- exercises/small_problems/adv1/script.js
+++ exercises/small_problems/adv1/script.js
@@ -1,6 +1,5 @@
 // Madlibs revisited: 
 
-//F6C7-0000-030D-647A
 
 var template1 = 'The ${adjective} brown ${noun} ${adverb} ' +
                 '${verb} the ${adjective} yellow ' +
@@ -60,4 +59,40 @@ function rotate(input) {
 
 //Merge sorted lists: this function should take two arrays
 
-// 
+function merge(array1, array2) {
+  var copy1 = array1.slice();
+  var copy2 = array2.slice();
+  var result = [];
+
+  while (copy1.length > 0 && copy2.length > 0) {
+    result.push(copy1[0] <= copy2[0] ? copy1.shift() : copy2.shift());
+  }
+
+  return result.concat(copy1.length === 0 ? copy2 : copy1);
+}
+
+// Binary search, uses the binary searching method to find the index of a searched item.  Otherwise returns -1.
+
+function binarySearch(arr, searchTerm) {
+	var end = arr.length - 1;
+	var start = 0;
+	var mid;
+
+	while (start <= end) {
+    mid = start + Math.floor((end - start) / 2);
+    if (array[mid] === searchItem) {
+      return mid;
+    } else if (array[mid] < searchItem) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+
+}
+
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 77);                                // -1
+binarySearch([1, 5, 7, 11, 23, 45, 65, 89, 102], 89);                                // 7
+binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Peter'); // -1
+binarySearch(['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'], 'Tyler'); // 6
